50c711e2486901c4777bbe0668de80169b6b2e34
王恒 <1066026709@qq.com>
2025-07-19 16:16:01 +0800

    1.0.0版本更新

---
+++ AWHConnectSocket.framework/Headers/AWHConnectSocket.h
@@ -0,0 +1,17 @@
+//
+//  AWHConnectSocket.h
+//  AWHConnectSocket
+//
+//  Created by 王恒 on 2024/7/14.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AWHConnectSocket.
+FOUNDATION_EXPORT double AWHConnectSocketVersionNumber;
+
+//! Project version string for AWHConnectSocket.
+FOUNDATION_EXPORT const unsigned char AWHConnectSocketVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AWHConnectSocket/PublicHeader.h>
+
